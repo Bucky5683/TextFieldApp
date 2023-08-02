d316b3e2fb3949b6aae94f2384fb6941d6c5f311
Bucky <bucky5683@gmail.com>
2023-08-03 00:14:12 +0900

    Make Layout

--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setup()
+    }
+    
+    func setup(){
+        textLabel.text = "텍스트를 입력해주세요."
+        view.backgroundColor = UIColor.gray //뷰컨트롤러 전체의 배경색상을 회색으로 변경
     }
 
-
+    @IBAction func doneButtonTapped(_ sender: UIButton) {
+        
+    }
+    
 }
 
